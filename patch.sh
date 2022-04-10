7796b7075a2670cf42b1d2270db2b8f9c2d74d85
notabucketofspam <not.a.bucket.of.spam@yandex.com>
2022-04-10 18:35:12 -0400

    Actually fix patch script

--- patch.sh
+++ patch.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
-echo -en "#include <string.h>\\n$(<./FreeRTOS/miniAVRfreeRTOS/hooks.c)\\n"\
->./FreeRTOS/miniAVRfreeRTOS/hooks.c
+sed -i 1i"#include <string.h>" ./FreeRTOS/miniAVRfreeRTOS/hooks.c
 sed -i s,"#warning \"Watchdog Timer used for scheduler.\"","//#warning \"Watchdog Timer used for scheduler.\"", \
 ./FreeRTOS/miniAVRfreeRTOS/port.c
-echo -en "#include <Arduino.h>\\n$(<./FastLED/FastLED/src/FastLED.h)\\n"\
->./FastLED/FastLED/src/FastLED.h
-echo -en "#include <Arduino.h>\\n$(<./Adafruit_NeoPixel/Adafruit_NeoPixel/Adafruit_NeoPixel.h)\\n"\
->./Adafruit_NeoPixel/Adafruit_NeoPixel/Adafruit_NeoPixel.h
+sed -i 1i"#include <Arduino.h>" ./Adafruit_NeoPixel/Adafruit_NeoPixel/Adafruit_NeoPixel.h
+sed -i 1i"#include <Arduino.h>" ./FastLED/FastLED/src/FastLED.h
