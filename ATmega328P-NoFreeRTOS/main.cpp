f34d39ace5eaddcc0e0122e1b98a28ceb4d229c3
notabucketofspam <not.a.bucket.of.spam@yandex.com>
2022-05-08 21:09:02 -0400

    Update fan configuration
    For use with Arctic BioniX P120 A-RGB fans

--- ATmega328P-NoFreeRTOS/main.cpp
+++ ATmega328P-NoFreeRTOS/main.cpp
@@ -12,18 +12,19 @@
 // ARGB config
 #define FRAMETIME_US 1000000UL/75
 #define FANS_OUT_PIN 9
-#define FANS_LED_CT 9
-CRGB leds_Fans[FANS_LED_CT];
+#define FAN_LED_CT 12
+#define FAN_CT 3
+CRGB leds_Fans[FAN_LED_CT*FAN_CT];
 #define BORDER_OUT_PIN 10
 #define BORDER_LED_CT 94
 CRGB leds_Border[BORDER_LED_CT];
 #define BRACKET_OUT_PIN 6
 #define BRACKET_LED_CT 6
 CRGB leds_Bracket[BRACKET_LED_CT];
-#define FILL_SMOOTHIE(leds, led_ct, offset) \
-  for (uint8_t px = 0; px < (led_ct); ++px) (leds)[px].setHue(px*UINT8_MAX/(led_ct) + (offset))
+#define FILL_SMOOTHIE(leds, start, led_ct, offset) \
+  for (uint8_t px = 0; px < (led_ct); ++px) (leds)[px + start].setHue(px*UINT8_MAX/(led_ct) + (offset))
 int main(void) {
-  FastLED.addLeds<NEOPIXEL, FANS_OUT_PIN>(leds_Fans, FANS_LED_CT);
+  FastLED.addLeds<NEOPIXEL, FANS_OUT_PIN>(leds_Fans, FAN_LED_CT*FAN_CT);
   FastLED.addLeds<NEOPIXEL, BORDER_OUT_PIN>(leds_Border, BORDER_LED_CT);
   FastLED.addLeds<NEOPIXEL, BRACKET_OUT_PIN>(leds_Bracket, BRACKET_LED_CT);
   FastLED.setMaxRefreshRate(0);
@@ -31,9 +32,11 @@ int main(void) {
   blink_isr_enable();
   for (;;) {
     _delay_us(FRAMETIME_US);
-    FILL_SMOOTHIE(leds_Fans, FANS_LED_CT, offset);
-    FILL_SMOOTHIE(leds_Border, BORDER_LED_CT, offset);
-    FILL_SMOOTHIE(leds_Bracket, BRACKET_LED_CT, offset);
+    FILL_SMOOTHIE(leds_Fans, 0, FAN_LED_CT, offset);
+    FILL_SMOOTHIE(leds_Fans, FAN_LED_CT, FAN_LED_CT, -offset);
+    FILL_SMOOTHIE(leds_Fans, 2*FAN_LED_CT, FAN_LED_CT, offset + 0x80);
+    FILL_SMOOTHIE(leds_Border, 0, BORDER_LED_CT, offset);
+    FILL_SMOOTHIE(leds_Bracket, 0, BRACKET_LED_CT, offset);
     ++offset;
     FastLED.show();
   }
