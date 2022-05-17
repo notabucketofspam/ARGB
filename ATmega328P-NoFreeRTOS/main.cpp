564ab0ebcd4c5879cd4f1e9b1c66f268a65ba9b4
notabucketofspam <not.a.bucket.of.spam@yandex.com>
2022-05-17 14:42:55 -0400

    Split fans into two sections
    Allows for each fan to be individually addressed

--- ATmega328P-NoFreeRTOS/main.cpp
+++ ATmega328P-NoFreeRTOS/main.cpp
@@ -11,10 +11,12 @@
 #include <FastLED.h>
 // ARGB config
 #define FRAMETIME_US 1000000UL/75
-#define FANS_OUT_PIN 9
 #define FAN_LED_CT 12
 #define FAN_CT 3
-CRGB leds_Fans[FAN_LED_CT*FAN_CT];
+#define FANS_IN_OUT_PIN 10
+CRGB leds_FansIn[FAN_LED_CT*FAN_CT];
+#define FANS_EX_OUT_PIN 3
+CRGB leds_FansEx[FAN_LED_CT*FAN_CT];
 #define BORDER_OUT_PIN 11
 #define BORDER_LED_CT 94
 CRGB leds_Border[BORDER_LED_CT];
@@ -24,7 +26,8 @@ CRGB leds_Bracket[BRACKET_LED_CT];
 #define FILL_SMOOTHIE(leds, start, led_ct, offset) \
   for (uint8_t px = 0; px < (led_ct); ++px) (leds)[px + start].setHue(px*UINT8_MAX/(led_ct) + (offset))
 int main(void) {
-  FastLED.addLeds<NEOPIXEL, FANS_OUT_PIN>(leds_Fans, FAN_LED_CT*FAN_CT);
+  FastLED.addLeds<NEOPIXEL, FANS_IN_OUT_PIN>(leds_FansIn, FAN_LED_CT*FAN_CT);
+  FastLED.addLeds<NEOPIXEL, FANS_EX_OUT_PIN>(leds_FansEx, FAN_LED_CT*FAN_CT);
   FastLED.addLeds<NEOPIXEL, BORDER_OUT_PIN>(leds_Border, BORDER_LED_CT);
   FastLED.addLeds<NEOPIXEL, BRACKET_OUT_PIN>(leds_Bracket, BRACKET_LED_CT);
   FastLED.setMaxRefreshRate(0);
@@ -32,9 +35,12 @@ int main(void) {
   blink_isr_enable();
   for (;;) {
     _delay_us(FRAMETIME_US);
-    FILL_SMOOTHIE(leds_Fans, 0, FAN_LED_CT, offset);
-    FILL_SMOOTHIE(leds_Fans, FAN_LED_CT, FAN_LED_CT, -offset);
-    FILL_SMOOTHIE(leds_Fans, 2*FAN_LED_CT, FAN_LED_CT, offset + 0x80);
+    FILL_SMOOTHIE(leds_FansIn, 0, FAN_LED_CT, offset);
+    FILL_SMOOTHIE(leds_FansIn, FAN_LED_CT, FAN_LED_CT, -offset);
+    FILL_SMOOTHIE(leds_FansIn, 2*FAN_LED_CT, FAN_LED_CT, offset + 0x80);
+    FILL_SMOOTHIE(leds_FansEx, 0, FAN_LED_CT, offset);
+    FILL_SMOOTHIE(leds_FansEx, FAN_LED_CT, FAN_LED_CT, -offset);
+    FILL_SMOOTHIE(leds_FansEx, 2*FAN_LED_CT, FAN_LED_CT, offset + 0x80);
     FILL_SMOOTHIE(leds_Border, 0, BORDER_LED_CT, offset);
     FILL_SMOOTHIE(leds_Bracket, 0, BRACKET_LED_CT, offset);
     ++offset;
